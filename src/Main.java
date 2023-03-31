5c556b2d684a40358cc9b3e0a83dd02a58956d76
BRIGI MARTON <brigi.marton397@gmail.com>
2023-03-31 11:33:40 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is an experminet");
         System.out.println("Hello world!!!!");
+        //qwertyuiop
     }
 }
